36519015a0fb975a837ceaaf0f7958a516c7b82e
Jakearooney <jakeatrooney@gmail.com>
2022-11-21 17:45:09 +0000

    Added Puzzle wire compatability.

--- Assets/Scripts/PowerCheck.cs
+++ Assets/Scripts/PowerCheck.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74f6945c0a2fa8981d789c617d66e72c4e04bf355862ce45d99fb49b50f73816
-size 2500
+oid sha256:cbf136fc17aa90709f11536adcc62ee4820411cc2314361ceae884a0845fee1d
+size 2609
---
+++ Assets/Scripts/WireActivationChange.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d5429ce3f1d894fa654cd56b4ea16712661a611ae73dbc4dd73987acd5d562b7
+size 2055
