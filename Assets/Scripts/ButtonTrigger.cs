1625b2575520e1c1e483523b46c8699fa66c9758
Jakearooney <jakeatrooney@gmail.com>
2022-11-24 17:41:00 +0000

    Added a functional button and Simon Says, not yet randomised.

---
+++ Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2bd1be0ce308da013b116aa03514333747b2645f1259c16b690ac461699a66a
+size 1446
---
+++ Assets/Scripts/SimonSays.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a2b47ae2c5a473c48dd7d65697031767f214003b34ade135b5114e038f3c67d8
+size 1349
